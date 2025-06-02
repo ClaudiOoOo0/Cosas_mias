f33641faf174028674690a509c8310dc4f2ad624
ClaudiOoOo0 <claudiofloresgalarce@gmail.com>
2025-06-02 11:23:24 -0400

    cosas

---
+++ Clases/Programacion/Programacion Orientada a Objetos/ejercicio_1_poo.py
@@ -0,0 +1,9 @@
+#definir clase estudiante 
+class estudiante():
+    "Parametros para estudiante"
+    def __init__(self, rut, nombre, edad):
+        self.par1 = rut
+        self.par2 = nombre
+        self.par3 = edad
+
+
