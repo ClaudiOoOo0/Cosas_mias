c3eedf1db34e60a215c91aced8df7470ec4fb164
ClaudiOoOo0 <claudiofloresgalarce@gmail.com>
2025-06-03 12:41:43 -0400

    Ejemplo de clase

--- Clases/Programacion/Programacion Orientada a Objetos/ejercicio_1_poo.py
+++ Clases/Programacion/Programacion Orientada a Objetos/ejercicio_1_poo.py
@@ -1,9 +1,23 @@
 #definir clase estudiante 
-class estudiante():
-    "Parametros para estudiante"
-    def __init__(self, rut, nombre, edad):
-        self.par1 = rut
-        self.par2 = nombre
-        self.par3 = edad
+class Estudiante():
+    """
+    Parametros para estudiante
+    """
 
+    def __init__(self, rut = "00000000-0", nombre = "Estudiante", apellido = "Estudioso", edad = 0):
+        self.rut = rut
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
+    def describe(self):
+        print(f"""Estudiante:
+        Rut: {self.rut}
+        Nombre: {self.nombre} {self.apellido}
+        Edad: {self.edad}
+        """)
+
+est = Estudiante()
+est.describe
+est2 = Estudiante("21222602-9","Gabriel","Salazar","20")
+est2.describe
